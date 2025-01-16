37c9a85b39d9920123115cc13aac2abbbec0e9cb
KumJungMin <rmawjdals@naver.com>
2025-01-16 23:41:16 +0900

    add: change test image files

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,85 @@
 <script setup lang="ts">
-import Logo from "./assets/vue.svg";
-
-import LongTree from "./assets/city.jpg";
-import LongTree2 from "./assets/city-2.jpg";
-import LongTree3 from "./assets/city-3.jpg";
-import LongTree4 from "./assets/city-4.jpg";
-import LongTree5 from "./assets/city-5.jpg";
-
-import VueLogo from "./assets/vue.png";
-import VueLogo2 from "./assets/vue-2.png";
-import VueLogo3 from "./assets/vue-3.png";
-import VueLogo4 from "./assets/vue-4.png";
-
-import SmallVueLogo from "./assets/small-vue.png";
-import SmallVueLogo2 from "./assets/small-vue-2.png";
-import SmallVueLogo3 from "./assets/small-vue-3.png";
-import SmallVueLogo4 from "./assets/small-vue-4.png";
-import SmallVueLogo5 from "./assets/small-vue-5.png";
+import File1 from "./assets/file-1.png";
+import File2 from "./assets/file-2.png";
+import File3 from "./assets/file-3.png";
+import File4 from "./assets/file-4.png";
+import File5 from "./assets/file-5.png";
+import File6 from "./assets/file-6.png";
+import File7 from "./assets/file-7.png";
+import File8 from "./assets/file-8.png";
+import File9 from "./assets/file-9.png";
+import File10 from "./assets/file-10.png";
+import File11 from "./assets/file-11.png";
+import File12 from "./assets/file-12.png";
+import File13 from "./assets/file-13.png";
+import File14 from "./assets/file-14.png";
+import File15 from "./assets/file-15.png";
+import File16 from "./assets/file-16.png";
+import File17 from "./assets/file-17.png";
+import File18 from "./assets/file-18.png";
+import File19 from "./assets/file-19.png";
+import File20 from "./assets/file-20.png";
+import File21 from "./assets/file-21.png";
+import File22 from "./assets/file-22.png";
+import File23 from "./assets/file-23.png";
+import File24 from "./assets/file-24.png";
+import File25 from "./assets/file-25.png";
+import File26 from "./assets/file-26.png";
+import File27 from "./assets/file-27.png";
+import File28 from "./assets/file-28.png";
+import File29 from "./assets/file-29.png";
+import File30 from "./assets/file-30.png";
+import File31 from "./assets/file-31.png";
+import File32 from "./assets/file-32.png";
+import File33 from "./assets/file-33.png";
+import File34 from "./assets/file-34.png";
+import File35 from "./assets/file-35.png";
+import File36 from "./assets/file-36.png";
+import File37 from "./assets/file-37.png";
+import File38 from "./assets/file-38.png";
+import File39 from "./assets/file-39.png";
+import File40 from "./assets/file-40.png";
 </script>
 
 <template>
-  <img :src="Logo" alt="Vue logo" />
-  <img :src="LongTree" alt="Long tree" />
-  <img :src="LongTree2" alt="Long tree 2" />
-  <img :src="LongTree3" alt="Long tree 3" />
-  <img :src="LongTree4" alt="Long tree 4" />
-  <img :src="LongTree5" alt="Long tree 5" />
-
-  <img :src="VueLogo" alt="Vue logo" />
-  <img :src="VueLogo2" alt="Vue logo 2" />
-  <img :src="VueLogo3" alt="Vue logo 3" />
-  <img :src="VueLogo4" alt="Vue logo 4" />
-
-  <img :src="SmallVueLogo" alt="Small Vue logo" />
-  <img :src="SmallVueLogo2" alt="Small Vue logo 2" />
-  <img :src="SmallVueLogo3" alt="Small Vue logo 3" />
-  <img :src="SmallVueLogo4" alt="Small Vue logo 4" />
-  <img :src="SmallVueLogo5" alt="Small Vue logo 5" />
+  <img :src="File1" alt="file-1" />
+  <img :src="File2" alt="file-2" />
+  <img :src="File3" alt="file-3" />
+  <img :src="File4" alt="file-4" />
+  <img :src="File5" alt="file-5" />
+  <img :src="File6" alt="file-6" />
+  <img :src="File7" alt="file-7" />
+  <img :src="File8" alt="file-8" />
+  <img :src="File9" alt="file-9" />
+  <img :src="File10" alt="file-10" />
+  <img :src="File11" alt="file-11" />
+  <img :src="File12" alt="file-12" />
+  <img :src="File13" alt="file-13" />
+  <img :src="File14" alt="file-14" />
+  <img :src="File15" alt="file-15" />
+  <img :src="File16" alt="file-16" />
+  <img :src="File17" alt="file-17" />
+  <img :src="File18" alt="file-18" />
+  <img :src="File19" alt="file-19" />
+  <img :src="File20" alt="file-20" />
+  <img :src="File21" alt="file-21" />
+  <img :src="File22" alt="file-22" />
+  <img :src="File23" alt="file-23" />
+  <img :src="File24" alt="file-24" />
+  <img :src="File25" alt="file-25" />
+  <img :src="File26" alt="file-26" />
+  <img :src="File27" alt="file-27" />
+  <img :src="File28" alt="file-28" />
+  <img :src="File29" alt="file-29" />
+  <img :src="File30" alt="file-30" />
+  <img :src="File31" alt="file-31" />
+  <img :src="File32" alt="file-32" />
+  <img :src="File33" alt="file-33" />
+  <img :src="File34" alt="file-34" />
+  <img :src="File35" alt="file-35" />
+  <img :src="File36" alt="file-36" />
+  <img :src="File37" alt="file-37" />
+  <img :src="File38" alt="file-38" />
+  <img :src="File39" alt="file-39" />
+  <img :src="File40" alt="file-40" />
 </template>

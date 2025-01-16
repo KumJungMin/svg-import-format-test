662e4b901b8aaf3c07fed3ff82182d64ae7fb0c5
KumJungMin <rmawjdals@naver.com>
2025-01-16 23:18:16 +0900

    feat: add assets image to test build time, size, cache etc

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Logo from "./assets/vue.svg";
+
+import LongTree from "./assets/city.jpg";
+import LongTree2 from "./assets/city-2.jpg";
+import LongTree3 from "./assets/city-3.jpg";
+import LongTree4 from "./assets/city-4.jpg";
+import LongTree5 from "./assets/city-5.jpg";
+
+import VueLogo from "./assets/vue.png";
+import VueLogo2 from "./assets/vue-2.png";
+import VueLogo3 from "./assets/vue-3.png";
+import VueLogo4 from "./assets/vue-4.png";
+
+import SmallVueLogo from "./assets/small-vue.png";
+import SmallVueLogo2 from "./assets/small-vue-2.png";
+import SmallVueLogo3 from "./assets/small-vue-3.png";
+import SmallVueLogo4 from "./assets/small-vue-4.png";
+import SmallVueLogo5 from "./assets/small-vue-5.png";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <img :src="Logo" alt="Vue logo" />
+  <img :src="LongTree" alt="Long tree" />
+  <img :src="LongTree2" alt="Long tree 2" />
+  <img :src="LongTree3" alt="Long tree 3" />
+  <img :src="LongTree4" alt="Long tree 4" />
+  <img :src="LongTree5" alt="Long tree 5" />
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+  <img :src="VueLogo" alt="Vue logo" />
+  <img :src="VueLogo2" alt="Vue logo 2" />
+  <img :src="VueLogo3" alt="Vue logo 3" />
+  <img :src="VueLogo4" alt="Vue logo 4" />
+
+  <img :src="SmallVueLogo" alt="Small Vue logo" />
+  <img :src="SmallVueLogo2" alt="Small Vue logo 2" />
+  <img :src="SmallVueLogo3" alt="Small Vue logo 3" />
+  <img :src="SmallVueLogo4" alt="Small Vue logo 4" />
+  <img :src="SmallVueLogo5" alt="Small Vue logo 5" />
+</template>

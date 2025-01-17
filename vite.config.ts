059a71c40e99ab02610e198eccc8fa5e2a33a825
KumJungMin <today.as.fresh@gmail.com>
2025-01-17 10:31:35 +0900

    feat: dynamic image import

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   build: {
-    assetsInlineLimit: 1024 * 9, // 기본값은 4096KB임
-  },
+    // assetsInlineLimit: 1024 * 500, // 기본값은 4096KB임
+    assetsInlineLimit: 1
+  }
 });

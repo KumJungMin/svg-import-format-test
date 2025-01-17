9c86cc94e4aeef78493e21113bac219dcddcbe76
KumJungMin <rmawjdals@naver.com>
2025-01-17 23:20:51 +0900

    chore: set rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    visualizer({
+      filename: "stats.html", // 결과물 파일명
+      template: "treemap", // 또는 'sunburst', 'network' 등
+      gzipSize: true, // gzip 사이즈도 표시
+    }),
+  ],
   build: {
-    assetsInlineLimit: 1024 * 9, // 기본값은 4096KB임
+    assetsInlineLimit: 1024 * 1, // 기본값은 4096KB임
   },
 });
